91dff8202764b99fc651560c50466e8fb01b0f2b
jimin9703 <moses0322@naver.com>
2024-10-07 09:25:30 +0900

    feat:[김지민] 결과 보고서 서비스를 위한 기본 구성을 구축합니다.[NFV-30]

---
+++ src/resultReport/router/ResultReportRouters.ts
@@ -0,0 +1,11 @@
+import ResultReportPage from "@/resultReport/pages/ResultReportPage.vue"
+
+const ResultReportRoutes = [
+    {
+        path: "/result-report/",
+        name: "ResultReportPage",
+        component: ResultReportPage,
+    },
+]
+
+export default ResultReportRoutes
