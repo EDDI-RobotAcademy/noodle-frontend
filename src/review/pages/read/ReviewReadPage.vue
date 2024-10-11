cf8bb6cf82eaa664cdf55e5aab8e5b60fa784569
Meue-L <meue7785@gmail.com>
2024-10-11 14:23:28 +0900

    init: [MinJaeLee/Meue] review read page 초기 셋업 [NFV-38]

---
+++ src/review/pages/read/ReviewReadPage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="isSelectionReview">selectionReview</div>
+    <div v-else>writingReview</div>
+</template>
+
+<script>
+export default {
+    props: {
+        id: {
+            type: Number,
+            required: true,
+        }
+    },
+    data() {
+        return {
+            isSelectionReview: true,
+            reviewTitle: '',
+            reviewWriter: '',
+            reviewContent: '',
+            designScore: 0,
+            usabilityScore: 0,
+            qualityScore: 0,
+            responsiveScore: 0,
+        }
+    }
+}
+</script>
+
+<style scoped></style>
