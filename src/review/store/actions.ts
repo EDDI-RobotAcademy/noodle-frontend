cf9176fb71a4228f51c4cf0c4ab2aa1332c1104a
Meue-L <meue7785@gmail.com>
2024-10-12 01:35:33 +0900

    feat: [MinJaeLee/Meue] 상세 리뷰 확인 페이지 구현 완료 [NFV-38]

--- src/review/store/actions.ts
+++ src/review/store/actions.ts
@@ -20,6 +20,10 @@ export type ReviewActions = {
 		context: ActionContext<any, any>,
 		payload: { userToken: string; ratingList: [number]; content: string }
 	): Promise<void>;
+	requestReviewDetailsToDjango(
+		context: ActionContext<any, any>,
+		reviewID: number
+	): Promise<void>;
 };
 
 const actions: ReviewActions = {
@@ -28,7 +32,6 @@ const actions: ReviewActions = {
 		payload: { pagination: number; perPage: number }
 	): Promise<void> {
 		try {
-			console.log("payload", payload);
 			const res = await axiosInst.djangoAxiosInst.post(
 				"/review/list",
 				payload
@@ -43,11 +46,9 @@ const actions: ReviewActions = {
 		context: ActionContext<any, any>
 	): Promise<void> {
 		try {
-			console.log("getting list initializing");
 			const res = await axiosInst.djangoAxiosInst.post(
 				"/review/entire-count"
 			);
-			console.log("entirecount", res.data);
 			return res.data.count;
 		} catch (error) {
 			console.error("requestEntireReviewListCount():" + error);
@@ -59,7 +60,6 @@ const actions: ReviewActions = {
 		payload: { title: string; userToken: string; content: string }
 	): Promise<void> {
 		try {
-			console.log("requesting register your review!");
 			await axiosInst.djangoAxiosInst.post(
 				"/review/register/writingReview",
 				payload
@@ -74,13 +74,27 @@ const actions: ReviewActions = {
 		payload: { userToken: string; ratingList: [number]; content: string }
 	): Promise<void> {
 		try {
-			console.log("requesting register your review!");
 			await axiosInst.djangoAxiosInst.post(
 				"/review/register/selectionReview",
 				payload
 			);
 		} catch (error) {
 			console.error("error occured while registering!" + error);
+			throw error;
+		}
+	},
+	async requestReviewDetailsToDjango(
+		context: ActionContext<any, any>,
+		reviewID: number
+	): Promise<void> {
+		try {
+			const res = await axiosInst.djangoAxiosInst.post(
+				"/review/read",
+				reviewID
+			);
+			return res.data;
+		} catch (error) {
+			console.error("error occured while getting details!" + error);
 			throw error;
 		}
 	},
