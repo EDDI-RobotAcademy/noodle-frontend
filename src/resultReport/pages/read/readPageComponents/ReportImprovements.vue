f90b7e8e3ed2ebc628f56ffe6c353146edda0a90
lleehs <lhs3789@gmail.com>
2024-10-17 14:39:20 +0900

    init: [이현석] 결과 보고서 중 ‘보완할 점’영역을 별도의 vue파일로 Components화 합니다. [NFV-63]
    init: [이현석] 결과 보고서 중 ‘보완할 점’영역을 별도의 vue파일로 Components화 합니다. [NFV-63]

---
+++ src/resultReport/pages/read/readPageComponents/ReportImprovements.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card-text>
+    <h2 class="text-h5 mb-4">보완할 점</h2>
+    <v-list dense>
+      <v-list-item v-for="(improvement, index) in improvements" :key="index">
+        <v-list-item-content>
+          <v-list-item-title>{{ improvement }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-card-text>
+</template>
+
+<script>
+export default {
+  name: 'ReportImprovements',
+  props: {
+    improvements: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
