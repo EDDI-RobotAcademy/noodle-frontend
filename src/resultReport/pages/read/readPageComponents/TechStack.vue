275dfe4480a471b8f576ea979f784afce1846e73
lleehs <lhs3789@gmail.com>
2024-10-17 14:30:04 +0900

    init: [이현석] 결과 보고서 중 ‘기술 스택’ 영역을 별도의 vue파일로 Components화 합니다. [NFV-60]
    init: [이현석] 결과 보고서 중 ‘기술 스택’ 영역을 별도의 vue파일로 Components화 합니다. [NFV-60]

---
+++ src/resultReport/pages/read/readPageComponents/TechStack.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card-text>
+    <h2 class="text-h5 mb-4">기술 스택</h2>
+    <v-chip-group>
+      <v-chip v-for="(tech, index) in techStack" :key="index" outlined>
+        {{ tech }}
+      </v-chip>
+    </v-chip-group>
+  </v-card-text>
+</template>
+
+<script>
+export default {
+  name: 'TechStack',
+  props: {
+    techStack: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
