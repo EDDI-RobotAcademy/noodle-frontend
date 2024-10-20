8f766d62d9b021c499a582bf972a3cb2c58fad10
lleehs <lhs3789@gmail.com>
2024-10-21 00:45:19 +0900

    init: [이현석] 새로운 Home화면을 구성합니다.[NFV-67]
    init: [이현석] 새로운 Home화면을 구성합니다.[NFV-67]

---
+++ src/Home/pages/ThirdHomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="Home_main">
+    <p>NEW HOME</p>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
