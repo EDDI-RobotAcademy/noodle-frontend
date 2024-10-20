509527795b661557b6ffd1105d893569294d3413
lleehs <lhs3789@gmail.com>
2024-10-21 01:09:29 +0900

    init: [이현석] Home화면에서 Footer영역인 HomeFifth components를 구성합니다. [NFV-72]
    init: [이현석] Home화면에서 Footer영역인 HomeFifth components를 구성합니다. [NFV-72]

---
+++ src/Home/pages/HomeComponents/HomeFifth.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Home_content_section4" data-aos="fade-up">
+    <h2>Projects</h2>
+    <p>Take a look at some of the successful projects that used our AI-powered report tool.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home_content_section4',
+};
+</script>
+
+<style scoped>
+.Home_content_section4 {
+  background-color: #ffffff;
+  text-align: center;
+}
+</style>
