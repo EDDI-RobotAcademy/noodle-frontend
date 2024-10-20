9c73db89801ea0a52293697cd52da616d2ace144
lleehs <lhs3789@gmail.com>
2024-10-21 01:01:26 +0900

    init: [이현석] Home화면에서 서비스 설명에 대한 영역인 HomeSecond components를 구성합니다. [NFV-69]
    init: [이현석] Home화면에서 서비스 설명에 대한 영역인 HomeSecond components를 구성합니다. [NFV-69]

---
+++ src/Home/pages/HomeComponents/HomeSecond.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Home_content_section1" data-aos="fade-left">
+    <h2>How it works</h2>
+    <p>Simply input your project details, and let our AI generate a comprehensive report for you.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home_content_section1',
+};
+</script>
+
+<style scoped>
+.Home_content_section1 {
+  background-color: #ffffff;
+  text-align: center;
+}
+</style>
