b3a095cef58ae31182bfe0a7f9abf40682a0cbf5
lleehs <lhs3789@gmail.com>
2024-10-21 01:03:57 +0900

    init: [이현석] Home화면에서 서비스 설명에 대한 영역인 HomeThird components를 구성합니다. [NFV-70]
    init: [이현석] Home화면에서 서비스 설명에 대한 영역인 HomeThird components를 구성합니다. [NFV-70]

---
+++ src/Home/pages/HomeComponents/HomeThird.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Home_content_section2" data-aos="zoom-in">
+    <h2>Contact Us</h2>
+    <p>Get in touch for more details or a demo of our AI report generation tool.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home_content_section2',
+};
+</script>
+
+<style scoped>
+.Home_content_section2 {
+  background-color: #f0f0f0;
+  text-align: center;
+}
+</style>
