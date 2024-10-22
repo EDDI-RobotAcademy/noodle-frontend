8b621b50a682704b31b75f7ebe2e1274860ecbac
Meue-L <meue7785@gmail.com>
2024-10-22 20:38:38 +0900

    refactor: [MinJaeLee/Meue] nuxt migration중, 추가 작업된 파일에 nuxt를 적용합니다 [NFV-76]

---
+++ nuxt-app/pages/components/HomeFifth.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="Home_content_section4" data-aos="fade-up">
+    <h2>Projects</h2>
+    <p>Take a look at some of the successful projects that used our AI-powered report tool.</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home_content_section4',
+})
+</script>
+
+<style scoped>
+.Home_content_section4 {
+  background-color: #ffffff;
+  text-align: center;
+}
+</style>
