7c310ef7ac13dc2302df050f847163371b244165
lleehs <lhs3789@gmail.com>
2024-10-25 10:36:06 +0900

    design: [이현석] HomeThird Components의 로드 AOS효과의 형식을 CSS->AOS라이브러리를 활용한 형식을 변경합니다. [NFV-80]
    design: [이현석] HomeThird Components의 로드 AOS효과의 형식을 CSS->AOS라이브러리를 활용한 형식을 변경합니다. [NFV-80]

--- src/Home/pages/HomeComponents/HomeThird.vue
+++ src/Home/pages/HomeComponents/HomeThird.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="home-third-body" ref="homeThird">
     <div class="left-content">
-      <p class="fade-in first" ref="firstText">NOODLE만이 가능한</p>
-      <p class="fade-in second" ref="secondText">개발자들을 위한 혁신</p>
+      <p id="first-text" 
+        data-aos="fade-in" 
+        data-aos-duration="2000">NOODLE만이 가능한</p>
+      <p id="second-text" 
+        data-aos="fade-in"
+        data-aos-delay="1000"
+        data-aos-anchor="#first-text"
+        data-aos-duration="2000">개발자들을 위한 혁신</p>
     </div>
 
-    <div class="right-content" :class="{ 'fade-up': showRightContent, 'hidden': !showRightContent }">
+    <div class="right-content" 
+        data-aos="fade-up" 
+        data-aos-delay="2000"
+        data-aos-anchor="#second-text"
+        data-aos-duration="1300">
       <div class="image-container"></div>
       <div class="text-container">
         <p>NOODLE은 개발자들이 개발에만 몰두할 수 있도록</p>
@@ -16,49 +26,11 @@
 </template>
 
 <script>
+import AOS from 'aos';
+
 export default {
   name: 'HomeThird',
-  data() {
-    return {
-      showRightContent: false,
-      animationStarted: false
-    }
-  },
-  mounted() {
-    const options = {
-      threshold: 0.3, // 30% 가 보일 때
-      rootMargin: '0px'
-    };
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting && !this.animationStarted) {
-          this.startAnimation();
-          this.animationStarted = true;
-          observer.unobserve(entry.target); // 한 번 실행 후 관찰 중단
-        }
-      });
-    }, options);
-
-    observer.observe(this.$refs.homeThird);
-  },
-  methods: {
-    startAnimation() {
-      // 첫 번째 텍스트 애니메이션 시작
-      this.$refs.firstText.style.animationPlayState = 'running';
-
-      // 첫 번째 텍스트 애니메이션 완료 감지
-      this.$refs.firstText.addEventListener('animationend', () => {
-        this.$refs.secondText.style.animationDelay = '0s';
-        this.$refs.secondText.style.animationPlayState = 'running';
-      });
-
-      // 두 번째 텍스트 애니메이션 완료 감지
-      this.$refs.secondText.addEventListener('animationend', () => {
-        this.showRightContent = true;
-      });
-    }
-  }
 };
 </script>
 
@@ -132,41 +104,4 @@ export default {
   padding-bottom: 50px;
 }
 
-@keyframes fadeIn {
-  from { opacity: 0; }
-  to { opacity: 1; }
-}
-
-.fade-in {
-  opacity: 0;
-  animation: fadeIn 1s ease-in forwards;
-  animation-play-state: paused; /* 모든 애니메이션 일시 정지 상태로 시작 */
-}
-
-.first {
-  animation-play-state: paused; /* first 클래스도 일시 정지로 시작 */
-}
-
-.hidden {
-  opacity: 0;
-  visibility: hidden;
-}
-
-.fade-up {
-  opacity: 0;
-  transform: translateY(20px);
-  animation: fadeUp 1s ease-out forwards;
-  visibility: visible;
-}
-
-@keyframes fadeUp {
-  from {
-    opacity: 0;
-    transform: translateY(20px);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-}
 </style>
