41f62af103cc8660c2c2ef11a021351ee67c0d64
lleehs <lhs3789@gmail.com>
2024-10-25 15:23:47 +0900

    feat: [이현석] HomeFourth에 메인 컨텐츠로 Report생성에 대한 이미지 및 초기 설명을 추가합니다. [NFV-89]
    feat: [이현석] HomeFourth에 메인 컨텐츠로 Report생성에 대한 이미지 및 초기 설명을 추가합니다. [NFV-89]

--- src/Home/pages/HomeComponents/HomeFourth.vue
+++ src/Home/pages/HomeComponents/HomeFourth.vue
@@ -1,24 +1,56 @@
 <template>
-  <div class="Home_content_section3" data-aos="fade-right">
-    <h2>Features</h2>
-    <ul>
-      <li>Automated report generation</li>
-      <li>Customizable templates</li>
-      <li>Project timeline integration</li>
-      <li>Multi-language support</li>
-    </ul>
+  <div class="home-fourth-body">
+
+    <div class="left-container">
+      <div class="image-container"></div>
+      <div class="text-container">
+        <p>초기 구성</p>
+        <p>초기 구성</p>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home_content_section3',
+  name: 'HomeFourth',
 };
 </script>
 
 <style scoped>
-.Home_content_section3 {
-  background-color: #e0e0e0;
-  text-align: center;
+.home-fourth-body {
+  display: flex;
+  background-color: #040202;
+  width: 100%;
+  height: 100vh;
 }
+
+.left-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  width: 50%;
+  height: 100%;
+}
+
+
+.image-container {
+  width: 80%;
+  height: 50%;
+  background-image: url('@/assets/images/fixed/1212.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  border-radius: 20px;
+}
+
+.text-container {
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+  padding-bottom: 50px;
+}
+
 </style>
