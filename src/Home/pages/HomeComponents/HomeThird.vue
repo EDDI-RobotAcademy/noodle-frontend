c56737a76c944f25a0256a74b0f1aafee3eae032
lleehs <lhs3789@gmail.com>
2024-10-27 21:40:21 +0900

    design: [이현석] HomeThird의 컨텐츠들을 재배치합니다. [NFV-70]
    design: [이현석] HomeThird의 컨텐츠들을 재배치합니다. [NFV-70]

--- src/Home/pages/HomeComponents/HomeThird.vue
+++ src/Home/pages/HomeComponents/HomeThird.vue
@@ -1,24 +1,75 @@
 <template>
+<div class="home-third">
   <div class="home-third-body">
-    <div class="background-image-container">
-      <div class="left-content" 
-          data-aos="fade-in">
-        <p>NOODLE만이 가능한</p>
-        <p>개발자들을 위한 혁신</p>
-      </div>
-    </div>
-    <div class="right-content" 
-        data-aos="fade-up" 
-        data-aos-delay="600"
-        >
-      <div class="image-container"></div>
+
+    <div class="content-container" data-aos="fade-left">
+      <v-card class="content-image" id="github-image"></v-card>
       <div class="text-container">
-        <p>NOODLE은 개발자들의 업무 효율을 높이기 위해</p>
-        <p>Backlog를 자동으로 생성해드리는 서비스를 제공하여</p>
-        <p>개발 업무의 혁신을 꿈꿉니다.</p>
+        <p class="number-icon">1</p>
+        <div class="text-title-container">
+          <p class="text-title">편리한 기능을 손쉽게 이용</p>
+          <p class="text-subtitle">누구든, 복잡한 회원가입 절차 없이 이용할 수 있도록</p>
+        </div>
+        <div class="text-content-container">
+          <p class="text-content">NOODLE에서는 복잡한 회원가입 절차 없이 누구나 쉽게 서비스를</p>
+          <p class="text-content">이용할 수 있도록 GitHub Login만으로 서비스를 이용하실 수 있습니다.</p>
+        </div>
       </div>
     </div>
+
+
+    <div class="content-container" data-aos="fade-right">
+      <div class="text-container">
+        <p class="number-icon">2</p>
+        <div class="text-title-container">
+          <p class="text-title">보고서를 간편하게 검색</p>
+          <p class="text-subtitle">원하는 보고서를 쉽게 검색할 수 있도록</p>
+        </div>
+        <div class="text-content-container">
+          <p class="text-content">오랜 기간동안 쌓여온 보고서들 중에서 내가 원하는 보고서를 찾는 건</p>
+          <p class="text-content">어려운 일이죠. NOODLE에선 검색을 통해 쉽게 찾을 수 있습니다.</p>
+        </div>
+      </div>
+      <v-card class="content-image" id="search-image"></v-card>
+    </div>
+
+
+    <div class="content-container" data-aos="fade-left">
+      <v-card class="content-image" id="backlog-image"></v-card>
+      <div class="text-container">
+        <p class="number-icon">3</p>
+        <div class="text-title-container">
+          <p class="text-title">혁신적인 Backlog자동 생성</p>
+          <p class="text-subtitle">개발자들과 원활한 협업을 할수 있도록</p>
+        </div>
+        <div class="text-content-container">
+          <p class="text-content">개발자와 협업이 필요하거나 개발자들끼리 협업을 할 때</p>
+          <p class="text-content">GitHub와의 연동을 통해 Project Code를 분석하여</p>
+          <p class="text-content">자동으로 Backlog를 생성해드리는 혁신적인 서비스를 제공합니다.</p>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="content-container" data-aos="fade-right">
+      <div class="text-container">
+        <p class="number-icon">4</p>
+        <div class="text-title-container">
+          <p class="text-title">AI를 통한 보고서 자동 생성</p>
+          <p class="text-subtitle">누구든, 업무에만 집중할 수 있도록</p>
+        </div>
+        <div class="text-content-container">
+          <p class="text-content">AI가 GitHub에 저장된 Code와 생성된 Backlog를 바탕으로 </p>
+          <p class="text-content">Project의 정량적 보고서를 자동으로 생성해드립니다.</p>
+        </div>
+      </div>
+      <v-card class="content-image" id="report-image"></v-card>
+    </div>
+
+    
+
   </div>
+</div>
 </template>
 
 <script>
@@ -27,91 +78,111 @@ import 'aos/dist/aos.css';
 
 export default {
   name: 'HomeTHird',
-  mounted() {
-    AOS.init({
-      once: false,
-      duration: 2000,
-      offset: 600,
-    });
-  }
 };
 </script>
 
 <style scoped>
-.home-third-body {
-  display: flex;
-  background-color: #040202;
+.home-third{
   width: 100%;
-  height: 100vh;
+  height: 350vh;
+  background-color: #080808;
+  display: flex;
+  justify-content: center;
 }
 
-.background-image-container {
-  width: 50%;
+
+.home-third-body {
+  width: 70%;
   height: 100%;
-  position: relative;
+  display: flex;
+  background-color: #080808;
+  display: flex;
+  flex-direction: column;
+  /* gap: 1%; */
+
 }
 
-.background-image-container::before {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-image: url('@/assets/images/fixed/homethird_image.jpg');
+.content-container{
+  width: 100%;
+  height: 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 60px;
+}
+
+.content-image{
+  width: 50%;
+  height: 60%;
+  border-radius: 25px;
+}
+
+#github-image{
+  background-image: url('@/assets/images/fixed/github_logo2.png');
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  opacity: 0.3;
-  z-index: 1;
 }
 
-.left-content {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-end;
-  padding-right: 100px;
-  padding-bottom: 100px;
-  position: relative;
-  overflow: hidden;
-  z-index: 2;
+#search-image{
+  background-image: url('@/assets/images/fixed/search.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 
-
-.left-content p {
-  font-size: 65px;
-  font-weight: bold;
-  color: #fff;
+#backlog-image{
+  background-image: url('@/assets/images/fixed/create_backlog.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 
-.right-content {
+#report-image{
+  background-image: url('@/assets/images/fixed/create_report.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.text-container{
   width: 50%;
-  height: 100%;
+  height: 80%;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: flex-start;
   gap: 30px;
 }
 
-.image-container {
-  width: 80%;
-  height: 50%;
-  background-image: url('@/assets/images/fixed/1212.jpg');
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  border-radius: 20px;
-}
-
-.text-container {
-  color: #fff;
-  font-size: 20px;
+.number-icon {
+  font-size: 40px;
   font-weight: bold;
-  padding-bottom: 50px;
+  color: #080808;
+  background-color: #ffffff;
+  width: 50px;
+  height: 50px;
+  border-radius: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
 }
 
+.text-title{
+  font-size: 50px;
+  font-weight: bold;
+  color: #ffffff;
+}
+
+.text-subtitle{
+  font-size: 22px;
+  font-weight: bold;
+  color: #ffffff;
+}
+
+.text-content{
+  font-size: 20px;
+  /* font-weight: bold; */
+  color: #ffffff;
+}
 </style>
