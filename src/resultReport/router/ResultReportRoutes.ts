29ad482e272f0393cc9dcf2fb451457c9fb568ab
lleehs <lhs3789@gmail.com>
2024-10-29 14:47:32 +0900

    add: [이현석] ReportListPage3의 라우터 추가 [NFV-40]
    add: [이현석] ReportListPage3의 라우터 추가 [NFV-40]

--- src/resultReport/router/ResultReportRoutes.ts
+++ src/resultReport/router/ResultReportRoutes.ts
@@ -4,6 +4,7 @@ import ResultReportReadPage from "@/resultReport/pages/read/ResultReportReadPage
 import ResultReportListPage2 from "../pages/list/ResultReportListPage2.vue"
 import ResultReportModifyPage from "../pages/modify/ResultReportModifyPage.vue"
 import ResultReportReadPage12 from "../pages/read/ResultReportReadPage12.vue"
+import ResultReportListPage3 from "@/resultReport/pages/list/ResultReportListPage3.vue"
 
 const ResultReportRoutes = [
     {
@@ -11,11 +12,15 @@ const ResultReportRoutes = [
         name: "ResultReportRegisterPage",
         component: ResultReportRegisterPage,
     },
-
     {
         path: "/report/list",
         name: "ResultReportListPage",
         component: ResultReportListPage
+    },
+    {
+        path: "/report/list3",
+        name: "ResultReportListPage3",
+        component: ResultReportListPage3
     },
     {
         path: "/report/read/:resultReportId",
